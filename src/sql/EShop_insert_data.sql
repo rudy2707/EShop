dc45fb18c65170f1c54c5f8f5dc0c5612e40aac2
Axel Fahy <axel@fahy.net>
2016-02-24 11:27:11 +0100

    feat : Script to insert data in DB
    Insert all the product Name and mesh filename into the 'tblProduct' table.

---
+++ src/sql/EShop_insert_data.sql
@@ -0,0 +1,36 @@
+-- -----------------------------------------------------
+-- Data for table `shop_test`.`tblProduct`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `shop_test`;
+INSERT INTO `shop_test`.`tblProduct` (`prodId`, `prodName`, `prodDescription`, `prodStock`, `prodPrice`, `prodMeshName`) VALUES (DEFAULT, 'Apple', 'This is juste a fucking\' apple', 1, 1, 'Apple.obj');
+INSERT INTO `shop_test`.`tblProduct` (`prodId`, `prodName`, `prodDescription`, `prodStock`, `prodPrice`, `prodMeshName`) VALUES (DEFAULT, 'Banana', 'Just for scale', 2, 2, 'BananaGroup.obj');
+INSERT INTO `shop_test`.`tblProduct` (`prodId`, `prodName`, `prodDescription`, `prodStock`, `prodPrice`, `prodMeshName`) VALUES (DEFAULT, 'BlueMilk', '', 1, 1, 'BlueMilk.obj');
+INSERT INTO `shop_test`.`tblProduct` (`prodId`, `prodName`, `prodDescription`, `prodStock`, `prodPrice`, `prodMeshName`) VALUES (DEFAULT, 'BrownEgg', '', 1, 1, 'BrownEgg.obj');
+INSERT INTO `shop_test`.`tblProduct` (`prodId`, `prodName`, `prodDescription`, `prodStock`, `prodPrice`, `prodMeshName`) VALUES (DEFAULT, 'CaramelDoughnut', '', 1, 1, 'CaramelDoughnut.obj');
+INSERT INTO `shop_test`.`tblProduct` (`prodId`, `prodName`, `prodDescription`, `prodStock`, `prodPrice`, `prodMeshName`) VALUES (DEFAULT, 'Carrot', '', 1, 1, 'Carrot.obj');
+INSERT INTO `shop_test`.`tblProduct` (`prodId`, `prodName`, `prodDescription`, `prodStock`, `prodPrice`, `prodMeshName`) VALUES (DEFAULT, 'Chocolate', '', 1, 1, 'Chocolate.obj');
+INSERT INTO `shop_test`.`tblProduct` (`prodId`, `prodName`, `prodDescription`, `prodStock`, `prodPrice`, `prodMeshName`) VALUES (DEFAULT, 'ChocolateDoughnut', '', 1, 1, 'ChocolateDoughnut.obj');
+INSERT INTO `shop_test`.`tblProduct` (`prodId`, `prodName`, `prodDescription`, `prodStock`, `prodPrice`, `prodMeshName`) VALUES (DEFAULT, 'Emmental', '', 1, 1, 'Emmental.obj');
+INSERT INTO `shop_test`.`tblProduct` (`prodId`, `prodName`, `prodDescription`, `prodStock`, `prodPrice`, `prodMeshName`) VALUES (DEFAULT, 'Fish', '', 1, 1, 'Fish.obj');
+INSERT INTO `shop_test`.`tblProduct` (`prodId`, `prodName`, `prodDescription`, `prodStock`, `prodPrice`, `prodMeshName`) VALUES (DEFAULT, 'GreenMilk', '', 1, 1, 'GreenMilk.obj');
+INSERT INTO `shop_test`.`tblProduct` (`prodId`, `prodName`, `prodDescription`, `prodStock`, `prodPrice`, `prodMeshName`) VALUES (DEFAULT, 'Gruyere', '', 1, 1, 'Gruyere.obj');
+INSERT INTO `shop_test`.`tblProduct` (`prodId`, `prodName`, `prodDescription`, `prodStock`, `prodPrice`, `prodMeshName`) VALUES (DEFAULT, 'Ham', '', 1, 1, 'Ham.obj');
+INSERT INTO `shop_test`.`tblProduct` (`prodId`, `prodName`, `prodDescription`, `prodStock`, `prodPrice`, `prodMeshName`) VALUES (DEFAULT, 'JamDoughnut', '', 1, 1, 'JamDoughnut.obj');
+INSERT INTO `shop_test`.`tblProduct` (`prodId`, `prodName`, `prodDescription`, `prodStock`, `prodPrice`, `prodMeshName`) VALUES (DEFAULT, 'Lolipop', '', 1, 1, 'Lolipop.obj');
+INSERT INTO `shop_test`.`tblProduct` (`prodId`, `prodName`, `prodDescription`, `prodStock`, `prodPrice`, `prodMeshName`) VALUES (DEFAULT, 'PaperTowel', '', 1, 1, 'PaperTowel.obj');
+INSERT INTO `shop_test`.`tblProduct` (`prodId`, `prodName`, `prodDescription`, `prodStock`, `prodPrice`, `prodMeshName`) VALUES (DEFAULT, 'Pepper', '', 1, 1, 'Pepper.obj');
+INSERT INTO `shop_test`.`tblProduct` (`prodId`, `prodName`, `prodDescription`, `prodStock`, `prodPrice`, `prodMeshName`) VALUES (DEFAULT, 'Pumpkin', '', 1, 1, 'Pumpkin.obj');
+INSERT INTO `shop_test`.`tblProduct` (`prodId`, `prodName`, `prodDescription`, `prodStock`, `prodPrice`, `prodMeshName`) VALUES (DEFAULT, 'RedMilk', '', 1, 1, 'RedMilk.obj');
+INSERT INTO `shop_test`.`tblProduct` (`prodId`, `prodName`, `prodDescription`, `prodStock`, `prodPrice`, `prodMeshName`) VALUES (DEFAULT, 'Scotch', '', 1, 1, 'Scotch.obj');
+INSERT INTO `shop_test`.`tblProduct` (`prodId`, `prodName`, `prodDescription`, `prodStock`, `prodPrice`, `prodMeshName`) VALUES (DEFAULT, 'Steak', '', 1, 1, 'Steak.obj');
+INSERT INTO `shop_test`.`tblProduct` (`prodId`, `prodName`, `prodDescription`, `prodStock`, `prodPrice`, `prodMeshName`) VALUES (DEFAULT, 'Toblerone', '', 1, 1, 'Toblerone.obj');
+INSERT INTO `shop_test`.`tblProduct` (`prodId`, `prodName`, `prodDescription`, `prodStock`, `prodPrice`, `prodMeshName`) VALUES (DEFAULT, 'ToiletPaper', '', 1, 1, 'ToiletPaper.obj');
+INSERT INTO `shop_test`.`tblProduct` (`prodId`, `prodName`, `prodDescription`, `prodStock`, `prodPrice`, `prodMeshName`) VALUES (DEFAULT, 'WhiteEgg', '', 1, 1, 'WhiteEgg.obj');
+INSERT INTO `shop_test`.`tblProduct` (`prodId`, `prodName`, `prodDescription`, `prodStock`, `prodPrice`, `prodMeshName`) VALUES (DEFAULT, 'YoghurtApple', '', 1, 1, 'YoghurtApple.obj');
+INSERT INTO `shop_test`.`tblProduct` (`prodId`, `prodName`, `prodDescription`, `prodStock`, `prodPrice`, `prodMeshName`) VALUES (DEFAULT, 'YoghurtNature', '', 1, 1, 'YoghurtNature.obj');
+INSERT INTO `shop_test`.`tblProduct` (`prodId`, `prodName`, `prodDescription`, `prodStock`, `prodPrice`, `prodMeshName`) VALUES (DEFAULT, 'YoghurtStrawberry', '', 1, 1, 'YoghurtStrawberry.obj');
+INSERT INTO `shop_test`.`tblProduct` (`prodId`, `prodName`, `prodDescription`, `prodStock`, `prodPrice`, `prodMeshName`) VALUES (DEFAULT, 'YoghurtVanilla', '', 1, 1, 'YoghurtVanilla.obj');
+
+COMMIT;
+
