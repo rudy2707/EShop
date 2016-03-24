415743b7d5740f7cb2e0467a7bcbe5951eda97a7
Pierre-Yves Kugler <pykugler@gmail.com>
2016-03-24 16:22:43 +0100

    header1stTest

--- src/public/shop.js
+++ src/public/shop.js
@@ -1,7 +1,6 @@
-angular
-  .module('app')
-  .controller('BasicDemoCtrl', function DemoCtrl($mdDialog) {
-    var originatorEv;
+angular.module('app')
+.controller('AppCtrl', function DemoCtrl($mdDialog) {
+  var originatorEv;
     this.openMenu = function($mdOpenMenu, ev) {
       originatorEv = ev;
       $mdOpenMenu(ev);
@@ -10,19 +9,37 @@ angular
     this.toggleNotifications = function() {
       this.notificationsEnabled = !this.notificationsEnabled;
     };
-    this.redial = function() {
+    this.profil = function() {
       $mdDialog.show(
         $mdDialog.alert()
           .targetEvent(originatorEv)
           .clickOutsideToClose(true)
           .parent('body')
-          .title('Suddenly, a redial')
-          .textContent('You just called a friend; who told you the most amazing story. Have a cookie!')
-          .ok('That was easy')
+          .title('Profil utilisateur')
+          .textContent('Ceci sera l \'affichage du profil de l\'utilisateur')
+          .ok('ok')
       );
       originatorEv = null;
     };
-    this.checkVoicemail = function() {
+    this.LogOut = function() {
+      $mdDialog.show(
+        $mdDialog.alert()
+          .targetEvent(originatorEv)
+          .clickOutsideToClose(true)
+          .parent('body')
+          .title('Log out')
+          .textContent('Voulez-vous vraiment vous déconnecter ?')
+          .cancel('non')
+          .ok('oui')
+      );
+      originatorEv = null;
       // This never happens.
     };
-  });
+
+});
+ 
+
+
+
+
+
